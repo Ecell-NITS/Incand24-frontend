146128608c117944a2b331f91594ab566e647522
Dhiraj <rd6999787@gmail.com>
2023-12-16 12:46:48 +0530

    hero-section updated

---
+++ app/components/HeroText/Loki.jsx
@@ -0,0 +1,64 @@
+'use client'
+import React from "react";
+import { useState, useEffect } from "react";
+import styles from "./Loki.module.scss";
+
+const Loki = () => {
+  const fonts = [
+    "Gilroy-light",
+    "Give You Glory",
+    "Geo",
+    "Gloria Hallelujah",
+    "Grey Qo",
+    "HeadlandOne",
+  ];
+
+  const [count, setCount] = useState(0);
+  const rollIntro = () => {
+    const letters = document.querySelectorAll(".letter");
+    letters.forEach((letter) => {
+      let randomFontIndex = Math.floor(Math.random() * fonts.length);
+      let randomFont = fonts[randomFontIndex];
+      letter.style.fontFamily = randomFont;
+    });
+  };
+
+  useEffect(() => {
+    const introAnimation = setInterval(() => {
+      rollIntro();
+      setCount((prevCount) => {
+        const newCount = prevCount + 1;
+        if (newCount > 8) {
+          clearInterval(introAnimation);
+          // const letters = document.querySelectorAll(".letter");
+          // letters.forEach((letter)=>{
+          //   letter.removeAttribute('style');
+          // })
+        }
+        return newCount;
+      });
+    }, 550);
+
+    return () => clearInterval(introAnimation);
+  }, []);
+
+  return (
+    <div className={styles.heroText}>
+      <h2 className={styles.loki}>
+        <p className={`${styles.letter} letter`}>C</p>
+        <p className={`${styles.letter} letter`}>O</p>
+        <p className={`${styles.letter} letter`}>M</p>
+        <p className={`${styles.letter} letter`}>I</p>
+        <p className={`${styles.letter} letter`}>N</p>
+        <p className={`${styles.letter} letter`}>G</p>
+        &nbsp;
+        <p className={`${styles.letter} letter`}>S</p>
+        <p className={`${styles.letter} letter`}>O</p>
+        <p className={`${styles.letter} letter`}>O</p>
+        <p className={`${styles.letter} letter`}>N</p>
+      </h2>
+    </div>
+  );
+};
+
+export default Loki;
--- app/page.js
+++ app/page.js
@@ -1,12 +1,16 @@
 import Link from "next/link";
 import Social from "./components/SocialHandles/Social";
 import styles from "./Home.module.scss";
+import Background from "./components/Backgroung/Background";
+import Loki from "./components/HeroText/Loki";
+import River from "./components/River/River";
 const Home = () => {
   return (
     <main className={styles.home}>
-      <h1>This is home page</h1>
-      <Link href="/about">About</Link>
-      <Social />
+      <Background>
+        <Loki/>
+        <River/>
+      </Background>
     </main>
   );
 };
