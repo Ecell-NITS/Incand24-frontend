0dd1a9d53b730759144e9c42b1b38acda993898a
bedanta <r4inr3aper@gmail.com>
2023-12-18 23:54:45 +0530

    test

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Link from "next/link";
+import Sponsor from "./components/Sponsor/Sponsor";
 
 const Home = () => {
   return (
     <main>
       <h1>This is home page</h1>
       <Link href="/about">About</Link>
+      <Sponsor />
     </main>
   );
 };
