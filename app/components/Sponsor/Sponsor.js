fe1417c276d0641ae430207a78ec3f4efc692960
Dhiraj <rd6999787@gmail.com>
2023-12-25 12:28:23 +0530

    img changes to IMAGE:Changes done

--- app/components/Sponsor/Sponsor.js
+++ app/components/Sponsor/Sponsor.js
@@ -1,26 +1,27 @@
 import React from "react";
+import Image from "next/image";
 import styles from "./Sponsor.module.css";
 const Sponsor = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.slider}>
         <div className={styles.slide1}>
-          <img src="https://res.cloudinary.com/dg27xsf6t/image/upload/c_scale,w_250,f_auto,fl_lossy/v1676282511/Sponsors/Amul_official_logo.svg_vhdzwt_ly8jey.png" />
-          <img src="https://res.cloudinary.com/dg27xsf6t/image/upload/c_scale,w_250,f_auto,fl_lossy/v1676282524/Sponsors/Bauli-01_q3dgek_clc3sl.png" />
-          <img src="https://upload.wikimedia.org/wikipedia/commons/f/f4/MTV_Music_2021_logo.svg" />
-          <img src="https://scontent.fjrh1-2.fna.fbcdn.net/v/t39.30808-6/301001425_592042485946389_4513685250550623066_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=frNdfUfWLUUAX9m4xUK&_nc_ht=scontent.fjrh1-2.fna&cb_e2o_trans=t&oh=00_AfCmwdvE_EPKaVBmU9nmtviqFk_vTGrOzBxKirA4W4L_BA&oe=65845E43" />
-          <img src="https://www.namhah.com/wp-content/uploads/2018/07/Gplulogo.jpg" />
-          <img src="https://res.cloudinary.com/dg27xsf6t/image/upload/c_scale,w_250,f_auto,fl_lossy/v1676282516/Sponsors/vector-coca-cola_dicy2e_rohrts.jpg" />
-          <img src="https://scontent.fjrh1-1.fna.fbcdn.net/v/t39.30808-6/305570063_471440231666074_6625266121703944169_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=WeoRzqVyi-4AX_sq-DE&_nc_oc=AQlqxS-PvsjSOWb2Mr3NANmYbFYQkjAA2mN96PD9CzASC9k5Xqh7QO_BC5k3M661vA-8YfSYs-_lDqVuIJo4Vyb5&_nc_ht=scontent.fjrh1-1.fna&cb_e2o_trans=t&oh=00_AfDqmK_fASFXTqmF6ab9tMwBnC--ZqYbtlUKvVpq_JgsmQ&oe=6585B546" />
+          <Image src="/sponsors/Amul.png" height={50} width={250} />
+          <Image src="/sponsors/cry.png" height={50} width={250} />
+          <Image src="/sponsors/Gplus.png" height={50} width={250} />
+          <Image src="/sponsors/nai.png" height={50} width={250} />
+          <Image src="/sponsors/Noticebar.png" height={50} width={250} />
+          <Image src="/sponsors/Gplus.png" height={50} width={250} />
+          <Image src="/sponsors/nai.png" height={50} width={250} />
         </div>
         <div className={styles.slide1}>
-          <img src="https://res.cloudinary.com/dg27xsf6t/image/upload/c_scale,w_250,f_auto,fl_lossy/v1676282511/Sponsors/Amul_official_logo.svg_vhdzwt_ly8jey.png" />
-          <img src="https://res.cloudinary.com/dg27xsf6t/image/upload/c_scale,w_250,f_auto,fl_lossy/v1676282524/Sponsors/Bauli-01_q3dgek_clc3sl.png" />
-          <img src="https://upload.wikimedia.org/wikipedia/commons/f/f4/MTV_Music_2021_logo.svg" />
-          <img src="https://scontent.fjrh1-2.fna.fbcdn.net/v/t39.30808-6/301001425_592042485946389_4513685250550623066_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=frNdfUfWLUUAX9m4xUK&_nc_ht=scontent.fjrh1-2.fna&cb_e2o_trans=t&oh=00_AfCmwdvE_EPKaVBmU9nmtviqFk_vTGrOzBxKirA4W4L_BA&oe=65845E43" />
-          <img src="https://www.namhah.com/wp-content/uploads/2018/07/Gplulogo.jpg" />
-          <img src="https://res.cloudinary.com/dg27xsf6t/image/upload/c_scale,w_250,f_auto,fl_lossy/v1676282516/Sponsors/vector-coca-cola_dicy2e_rohrts.jpg" />
-          <img src="https://scontent.fjrh1-1.fna.fbcdn.net/v/t39.30808-6/305570063_471440231666074_6625266121703944169_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=WeoRzqVyi-4AX_sq-DE&_nc_oc=AQlqxS-PvsjSOWb2Mr3NANmYbFYQkjAA2mN96PD9CzASC9k5Xqh7QO_BC5k3M661vA-8YfSYs-_lDqVuIJo4Vyb5&_nc_ht=scontent.fjrh1-1.fna&cb_e2o_trans=t&oh=00_AfDqmK_fASFXTqmF6ab9tMwBnC--ZqYbtlUKvVpq_JgsmQ&oe=6585B546" />
+          <Image src="/sponsors/Amul.png" height={50} width={250} />
+          <Image src="/sponsors/cry.png" height={50} width={250} />
+          <Image src="/sponsors/Gplus.png" height={50} width={250} />
+          <Image src="/sponsors/nai.png" height={50} width={250} />
+          <Image src="/sponsors/Noticebar.png" height={50} width={250} />
+          <Image src="/sponsors/Gplus.png" height={50} width={250} />
+          <Image src="/sponsors/nai.png" height={50} width={250} />
         </div>
       </div>
       <div className={styles.text}>
@@ -28,22 +29,22 @@ const Sponsor = () => {
       </div>
       <div className={styles.slider}>
         <div className={styles.slide2}>
-          <img src="https://res.cloudinary.com/dg27xsf6t/image/upload/c_scale,w_250,f_auto,fl_lossy/v1676282511/Sponsors/Amul_official_logo.svg_vhdzwt_ly8jey.png" />
-          <img src="https://res.cloudinary.com/dg27xsf6t/image/upload/c_scale,w_250,f_auto,fl_lossy/v1676282524/Sponsors/Bauli-01_q3dgek_clc3sl.png" />
-          <img src="https://upload.wikimedia.org/wikipedia/commons/f/f4/MTV_Music_2021_logo.svg" />
-          <img src="https://scontent.fjrh1-2.fna.fbcdn.net/v/t39.30808-6/301001425_592042485946389_4513685250550623066_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=frNdfUfWLUUAX9m4xUK&_nc_ht=scontent.fjrh1-2.fna&cb_e2o_trans=t&oh=00_AfCmwdvE_EPKaVBmU9nmtviqFk_vTGrOzBxKirA4W4L_BA&oe=65845E43" />
-          <img src="https://www.namhah.com/wp-content/uploads/2018/07/Gplulogo.jpg" />
-          <img src="https://res.cloudinary.com/dg27xsf6t/image/upload/c_scale,w_250,f_auto,fl_lossy/v1676282516/Sponsors/vector-coca-cola_dicy2e_rohrts.jpg" />
-          <img src="https://scontent.fjrh1-1.fna.fbcdn.net/v/t39.30808-6/305570063_471440231666074_6625266121703944169_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=WeoRzqVyi-4AX_sq-DE&_nc_oc=AQlqxS-PvsjSOWb2Mr3NANmYbFYQkjAA2mN96PD9CzASC9k5Xqh7QO_BC5k3M661vA-8YfSYs-_lDqVuIJo4Vyb5&_nc_ht=scontent.fjrh1-1.fna&cb_e2o_trans=t&oh=00_AfDqmK_fASFXTqmF6ab9tMwBnC--ZqYbtlUKvVpq_JgsmQ&oe=6585B546" />
+          <Image src="/sponsors/Amul.png" height={50} width={250} />
+          <Image src="/sponsors/cry.png" height={50} width={250} />
+          <Image src="/sponsors/Gplus.png" height={50} width={250} />
+          <Image src="/sponsors/nai.png" height={50} width={250} />
+          <Image src="/sponsors/Noticebar.png" height={50} width={250} />
+          <Image src="/sponsors/Gplus.png" height={50} width={250} />
+          <Image src="/sponsors/nai.png" height={50} width={250} />
         </div>
         <div className={styles.slide2}>
-          <img src="https://res.cloudinary.com/dg27xsf6t/image/upload/c_scale,w_250,f_auto,fl_lossy/v1676282511/Sponsors/Amul_official_logo.svg_vhdzwt_ly8jey.png" />
-          <img src="https://res.cloudinary.com/dg27xsf6t/image/upload/c_scale,w_250,f_auto,fl_lossy/v1676282524/Sponsors/Bauli-01_q3dgek_clc3sl.png" />
-          <img src="https://upload.wikimedia.org/wikipedia/commons/f/f4/MTV_Music_2021_logo.svg" />
-          <img src="https://scontent.fjrh1-2.fna.fbcdn.net/v/t39.30808-6/301001425_592042485946389_4513685250550623066_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=frNdfUfWLUUAX9m4xUK&_nc_ht=scontent.fjrh1-2.fna&cb_e2o_trans=t&oh=00_AfCmwdvE_EPKaVBmU9nmtviqFk_vTGrOzBxKirA4W4L_BA&oe=65845E43" />
-          <img src="https://www.namhah.com/wp-content/uploads/2018/07/Gplulogo.jpg" />
-          <img src="https://res.cloudinary.com/dg27xsf6t/image/upload/c_scale,w_250,f_auto,fl_lossy/v1676282516/Sponsors/vector-coca-cola_dicy2e_rohrts.jpg" />
-          <img src="https://scontent.fjrh1-1.fna.fbcdn.net/v/t39.30808-6/305570063_471440231666074_6625266121703944169_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=WeoRzqVyi-4AX_sq-DE&_nc_oc=AQlqxS-PvsjSOWb2Mr3NANmYbFYQkjAA2mN96PD9CzASC9k5Xqh7QO_BC5k3M661vA-8YfSYs-_lDqVuIJo4Vyb5&_nc_ht=scontent.fjrh1-1.fna&cb_e2o_trans=t&oh=00_AfDqmK_fASFXTqmF6ab9tMwBnC--ZqYbtlUKvVpq_JgsmQ&oe=6585B546" />
+          <Image src="/sponsors/Amul.png" height={50} width={250} />
+          <Image src="/sponsors/cry.png" height={50} width={250} />
+          <Image src="/sponsors/Gplus.png" height={50} width={250} />
+          <Image src="/sponsors/nai.png" height={50} width={250} />
+          <Image src="/sponsors/Noticebar.png" height={50} width={250} />
+          <Image src="/sponsors/Gplus.png" height={50} width={250} />
+          <Image src="/sponsors/nai.png" height={50} width={250} />
         </div>
       </div>
     </div>
