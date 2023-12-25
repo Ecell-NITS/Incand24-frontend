1cfae4be8024a181f5c2c9a695f5f7e82cb6f2bf
Swarup Chanda <swarupchanda1963@gmail.com>
2023-12-26 00:21:17 +0530

    Added Feat of Tree animation

--- app/components/hero/Hero.jsx
+++ app/components/hero/Hero.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 // eslint-disable-next-line camelcase
-import { Allura,Passion_One} from "next/font/google";
+import { Allura, Passion_One } from "next/font/google";
 import React from "react";
 import Image from "next/image";
 // eslint-disable-next-line import/no-extraneous-dependencies
@@ -9,23 +9,45 @@ import { motion } from "framer-motion";
 import styles from "./hero.module.scss";
 
 const passion = Passion_One({
-  weight: ["400"],   
+  weight: ["400"],
   display: "swap",
   subsets: ["latin"],
   variable: "--passion-font",
 });
 
-
 const poppins = Allura({
-  weight: ["400"],   
+  weight: ["400"],
   display: "swap",
   subsets: ["latin"],
   variable: "--allura-font",
 });
 const Hero = () => {
-  
   return (
     <div className={styles.wrapper}>
+      <motion.div
+        id={styles.leftTree}
+        initial={{
+          x: 0,
+        }}
+        animate={{
+          x: "-100%",
+        }}
+        transition={{ duration: 3 }}
+      >
+        <Image alt="left_Tree" src="/images/left_tree.png" layout="fill" />
+      </motion.div>
+      <motion.div
+        id={styles.rightTree}
+        initial={{
+          x: 0,
+        }}
+        animate={{
+          x: "100%",
+        }}
+        transition={{ duration: 3 }}
+      >
+        <Image alt="left_Tree" src="/images/right_tree.png" layout="fill" />
+      </motion.div>
       <motion.div
         id={styles.foreground}
         initial={{
@@ -36,17 +58,18 @@ const Hero = () => {
           y: 100,
           scale: 1.25,
         }}
-        transition={{ duration: 1.8 }}
+        transition={{ duration: 1.8, delay: 2 }}
       >
-        <Image alt="hero-bg" src="/images/Foreground.png" fill/>
+        <Image alt="hero-bg" src="/images/Foreground.png" fill />
       </motion.div>
       <motion.div
         animate={{
           scale: 2.5,
           z: -9,
           y: -500,
+          animationDelay: 1.8,
         }}
-        transition={{ duration: 1.8 }}
+        transition={{ duration: 1.8, delay: 2 }}
         id={styles.hills}
       >
         <Image alt="hero-bg" src="/images/Hills.png" fill />
@@ -62,16 +85,14 @@ const Hero = () => {
           scale: 1.6,
           top: -10,
         }}
-        transition={{ duration: 1.8 }}
+        transition={{ duration: 1.8, delay: 2 }}
         id={styles.background}
       >
         <Image alt="hero-bg" src="/images/Background.png" fill />
       </motion.div>
 
       {/* coming Soon text */}
-      <div
-        className={styles.textWrapper}
-      >
+      <div className={styles.textWrapper}>
         <h1 className={`${styles.heading} ${passion.className}`}>INCANDESCENCE‘24 </h1>
         <h4 className={`${styles.comingSoon} ${poppins.className}`}>COMING SOON...</h4>
       </div>
