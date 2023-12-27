e325a9a3c6a50de8c67af5803411055ee0786f0b
ruler45 <sahin0945@gmail.com>
2023-12-27 17:16:34 +0530

    about backgroud animated and some minor changes in socialHandle sidebar

--- app/components/SocialHandles/Social.jsx
+++ app/components/SocialHandles/Social.jsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react";
+
 import Image from "next/image";
 import styles from "./social.module.scss";
 
 const Social = () => {
+  const [scrollY, setScrollY] = useState(0);
+  const [social, setSocial] = useState(false);
+
+  const handleScroll = () => {
+    setScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    handleScroll();
+
+    window.addEventListener("scroll", handleScroll);
+    if (scrollY > window.innerHeight / 1.5) {
+      setSocial(true);
+    } else {
+      setSocial(false);
+    }
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [social, scrollY]);
+
   return (
-    <div className={`${styles.socialHandle} `}>
+    <div className={`${styles.socialHandle} ${social ? styles.visible : ""} `}>
       <div className={styles.socialIcon}>
         <a href="https://www.linkedin.com/company/incandescence23/" target="_blank">
           <Image width={40} height={40} src="/logos/linkedin.svg" alt="linkedin" />
--- app/page.js
+++ app/page.js
@@ -1,12 +1,15 @@
+"use client";
+
 import Sponsor from "./components/Sponsor/Sponsor";
-import Social from "./components/SocialHandles/Social";
 import styles from "./Home.module.scss";
 import About from "./components/about/About";
+import Social from "./components/SocialHandles/Social";
 
 const Home = () => {
   return (
     <main className={styles.home}>
       <About />
+
       <Sponsor />
       <Social />
     </main>
