2d120fba48e67bcecc9dceb9e1304a5ff4667702
Nandini <th.nandini04@gmail.com>
2023-12-29 10:49:30 +0530

    ADD:navbar

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Link from "next/link";
-import Social from "./components/SocialHandles/Social";
+// import Social from "./components/SocialHandles/Social";
 import styles from "./Home.module.scss";
+import Navbar from "./components/Navbar/Navbar";
 const Home = () => {
   return (
     <main className={styles.home}>
+      <Navbar />
       <h1>This is home page</h1>
       <Link href="/about">About</Link>
-      <Social />
+      {/* <Social /> */}
     </main>
   );
 };
