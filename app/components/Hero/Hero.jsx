ef4b0e6b876417797fe257c75d1af2e1f9a96282
Dhiraj <rd6999787@gmail.com>
2024-01-01 20:08:27 +0530

    brochure link change and some minor changes on navbar feat

--- app/components/Hero/Hero.jsx
+++ app/components/Hero/Hero.jsx
@@ -228,7 +228,6 @@ const Hero = () => {
             className={` ${passion.className} ${night ? styles.nightLogo : ""}`}
           >
             <Image
-            style={{zIndex: 4}}
               className={styles.light}
               src="/images/Logo.png"
               alt="logo"
@@ -236,7 +235,6 @@ const Hero = () => {
               sizes="!00%"
             />
             <Image
-      
               className={styles.dark}
               src="/images/LogoDark.png"
               alt="logo"
