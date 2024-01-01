1074ddbdb553f5985382565c68adcd29a1ae7529
hereisadi <hereisadi@proton.me>
2024-01-01 10:20:42 +0530

    UPD:about font family

--- app/components/about/About.jsx
+++ app/components/about/About.jsx
@@ -1,6 +1,7 @@
 // About Section Code: Suraj Verma
 
 import React from "react";
+import { Allura as allura, Poppins as poppins } from "next/font/google";
 import styles from "./About.module.scss";
 
 export const metadata = {
@@ -8,13 +9,23 @@ export const metadata = {
   description: "About",
 };
 
+const Allura = allura({
+  weight: ["400"],
+  subsets: ["latin"],
+});
+
+const Poppins = poppins({
+  weight: ["400"],
+  subsets: ["latin"],
+});
+
 const About = () => {
   return (
     <div className={styles.body} id="about">
       <div className={styles.branch}></div>
       <div className={styles.about_us}>
-        <div className={styles.heading}>About Us</div>
-        <div className={styles.content}>
+        <div className={`${styles.heading} ${Allura.className}`}>About Us</div>
+        <div className={`${styles.content} ${Poppins.className}`}>
           Incandescence, the cultural fest of NIT Silchar, is one of the most hyped and
           happening fest in the entire North East. The land of NITS witnesses some of the
           most famous artists of the nation, while the air of the place is filled with
@@ -27,8 +38,8 @@ const About = () => {
         <div className={styles.innerMandala}></div>
       </div>
       <div className={styles.about_nits}>
-        <div className={styles.heading}>About NIT Silchar</div>
-        <div className={styles.content}>
+        <div className={`${styles.heading} ${Allura.className}`}>About NIT Silchar</div>
+        <div className={`${styles.content} ${Poppins.className}`}>
           One of the most premiere institutions in the country, NITS is not just a place
           which shelters some of the best engineers of the future, but also one which is
           known for embracing each culture with equal respect and love, where every
