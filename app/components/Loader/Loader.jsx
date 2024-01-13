8414221788480e16f7834ac85eb8e06223e63411
hereisadi <hereisadi@proton.me>
2024-01-13 12:41:31 +0530

    UPD:loader

--- app/components/Loader/Loader.jsx
+++ app/components/Loader/Loader.jsx
@@ -1,16 +1,45 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Lottie from "lottie-react";
 import styles from "./Loader.module.scss";
 import animationData from "../../../public/loader.lottie";
 
-const LottieAnimation = () => {
+const LoaderAnimation = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const isFinishedLoading = () => {
+      if (document.readyState === "complete") {
+        setLoading(false);
+      }
+    };
+    isFinishedLoading();
+    document.addEventListener("readystatechange", isFinishedLoading);
+    return () => {
+      document.removeEventListener("readystatechange", isFinishedLoading);
+    };
+  }, []);
+
+  const loaderStyle = loading
+    ? {
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100vw",
+        height: "100vh",
+        overflow: "hidden",
+        zIndex: 9999,
+      }
+    : {};
+
   return (
-    <div className={styles.container}>
-      <Lottie animationData={animationData} loop autoplay />
+    <div style={loaderStyle}>
+      <div className={`${styles.container} ${loading ? styles.active : ""}`}>
+        <Lottie animationData={animationData} loop autoplay />
+      </div>
     </div>
   );
 };
 
-export default LottieAnimation;
+export default LoaderAnimation;
