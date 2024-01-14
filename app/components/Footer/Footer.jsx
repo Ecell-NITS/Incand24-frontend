36d0676068abf52ec458ecebe14858beebf4b6cd
hereisadi <hereisadi@proton.me>
2024-01-14 10:56:22 +0530

    minor chnages in footer

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -1,4 +1,6 @@
-import Link from "next/link";
+/* eslint-disable jsx-a11y/control-has-associated-label */
+
+import { Link } from "react-scroll";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Icon } from "@iconify/react";
 import Image from "next/image";
@@ -7,66 +9,70 @@ import hills from "../../../public/images/footerHills.png";
 import logo from "../../../public/images/LogoDark.png";
 import Brochure from "../Button/Button";
 
+export const metadata = {
+  title: "Footer",
+  description: "Footer",
+};
+
 const Footer = () => {
   return (
     <div className={styles.Footer}>
       <div className={styles.hills}>
-        <Image alt="" src={hills} fill />
+        <Image style={{ pointerEvents: "none" }} alt="" src={hills} fill />
       </div>
       <div className={styles.container}>
         <div className={styles.info}>
-          <Link
+          <a
             href="https://drive.google.com/file/d/1jA6Y5fh-ZW8VYepFH7QhS6UZus-KYgHC/view?usp=drivesdk"
             target="_blank"
             className={styles.brochure}
           >
             <Brochure />
-          </Link>
+          </a>
           <div className={styles.contact}>
             <div> Contact us</div>
-            <Link href="mailto:incandescene@nits.ac.in">
+            <a href="mailto:incandescene@nits.ac.in">
               <span>
                 <Icon icon="iconoir:mail" color="#acebbd" width="25" />
                 incandescene@nits.ac.in{" "}
               </span>
-            </Link>
+            </a>
           </div>
         </div>
         <div className={styles.logo}>
-          <Image alt=" " src={logo} fill />
+          <Link to="hero" spy smooth hashSpy offset={50} duration={500}>
+            <Image style={{ cursor: "pointer" }} alt=" " src={logo} fill />
+          </Link>
         </div>
         <div className={styles.info}>
           <div className={styles.follow}>
             Follow us
             <div className={styles.social}>
-              <Link
+              <a
                 href="https://www.instagram.com/incandescence.nitsilchar"
                 target="_blank"
               >
                 <Icon icon="mdi:instagram" color="#acebbd" width="30" />
-              </Link>
-              <Link
-                href="https://www.linkedin.com/company/incandescence23/"
-                target="_blank"
-              >
+              </a>
+              <a href="https://www.linkedin.com/company/incandescence23/" target="_blank">
                 <Icon icon="pajamas:linkedin" color="#acebbd" width="30" />
-              </Link>
-              <Link href="https://www.facebook.com/incandescence.nits/" target="_blank">
+              </a>
+              <a href="https://www.facebook.com/incandescence.nits/" target="_blank">
                 <Icon icon="fe:facebook" color="#acebbd" width="30" />
-              </Link>
+              </a>
             </div>
           </div>
           <div className={styles.contact}>
             <div>NIT SILCHAR</div>
             <span>
-              <Link target="_blank" href="https://maps.app.goo.gl/5vA3bZoxBSHCu38H6">
+              <a target="_blank" href="https://maps.app.goo.gl/5vA3bZoxBSHCu38H6">
                 {" "}
                 <div>
                   <Icon icon="system-uicons:location" color="#acebbd" width="25" />
                   NIT Road, Fakirtilla,
                 </div>
                 <div>Silchar, Assam 788010</div>
-              </Link>
+              </a>
             </span>
           </div>
         </div>
