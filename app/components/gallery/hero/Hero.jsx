d4354df64eedcae3befe3de114c861c139adee8b
Dhiraj <rd6999787@gmail.com>
2024-02-06 18:56:18 +0530

    video added

--- app/components/gallery/hero/Hero.jsx
+++ app/components/gallery/hero/Hero.jsx
@@ -5,7 +5,7 @@ import { Allura } from "next/font/google";
 import Image from "next/image";
 import { Player } from "@lottiefiles/react-lottie-player";
 import treeBg from "../../../../public/images/tree.png";
-import leaf from "../leafAnimation"
+import leaf from "../leafAnimation";
 import styles from "./Hero.module.scss";
 import GradientBg from "../../Shared/GradientBg/GradientBg";
 
@@ -19,15 +19,30 @@ const allura = Allura({
 const Hero = () => {
   return (
     <div className={styles.container}>
+      <GradientBg title="" position="absolute" />
 
-      <GradientBg title="" position="absolute"/>
-      
-      <Image src={treeBg} alt="tree" layout="responsive" height={1500} width={1500} className={styles.tree}/> 
+      <Image
+        src={treeBg}
+        alt="tree"
+        layout="responsive"
+        height={1500}
+        width={1500}
+        className={styles.tree}
+      />
 
       <div className={styles.contents}>
         <div>
-        <h1 className={allura.className}>Glimpse</h1>
-        <div className={styles.glimpse}></div>
+          <h1 className={allura.className}>Glimpse</h1>
+          <div className={styles.glimpse}>
+            <iframe
+              src="https://www.youtube.com/embed/YmjE5-_z9VM?si=ulxkQUyzFn0yqtQF&amp;controls=0&amp;start=4&amp;autoplay=1&amp;loop=1&amp;mute=1"
+              title="Incandenscence"
+              frameborder="0"
+              allow="accelerometer; autoplay; loop; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              loop
+
+            ></iframe>
+          </div>
         </div>
       </div>
 
@@ -41,7 +56,6 @@ const Hero = () => {
           style={{ height: "auto", width: "40vw" }}
         ></Player>
       </div>
-      
     </div>
   );
 };
